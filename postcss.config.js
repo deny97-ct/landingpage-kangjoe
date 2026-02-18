5f6af2936558575cd4f73a8316381d9a008d987b
deny97-ct <deny97@gmail.com>
2026-02-18 17:28:21 +0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
+    // Memproses direktif tailwind seperti @tailwind base, dsb.
     tailwindcss: {},
+    // Menambahkan vendor prefix otomatis agar tampilan rapi di semua browser (Chrome, Safari, Firefox)
     autoprefixer: {},
   },
 }
