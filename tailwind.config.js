82832eb9ea332bec86b21a91bfe5520091d4ffab
deny97-ct <deny97@gmail.com>
2026-02-18 17:14:30 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand: {
-          blue: "#3b82f6",
-          violet: "#8b5cf6",
-        }
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        blue: {
+          500: "#3b82f6",
+          600: "#2563eb",
+        },
+        violet: {
+          500: "#8b5cf6",
+          600: "#7c3aed",
+        },
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       },
     },
   },
