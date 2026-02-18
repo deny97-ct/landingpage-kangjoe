3a47e7c1059c25566bd4e4be2eb6d4712492226e
deny97-ct <deny97@gmail.com>
2026-02-18 17:05:07 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          blue: "#3b82f6",
+          violet: "#8b5cf6",
+        }
+      },
+    },
+  },
+  plugins: [],
+}
